2fb149f77a6e8f98f6b172edd4c0feab0abc3709
muvox <muvoksi@gmail.com>
2019-05-10 21:08:51 +0300

    Finally got images to show up, next is proper table iteration

--- bingo.js
+++ bingo.js
@@ -6,11 +6,107 @@ var arrayOfObject = [
   {
     name: 'Jon Snow',
     image: 'images/jonSnow.png'
+  },
+  {
+    name: 'Bran Stark',
+    image: 'images/branStark.png'
+  },
+  {
+    name: 'Brianne Of Tarth',
+    image: 'images/brianneOfTarth.png'
+  },
+  {
+    name: 'Bronn',
+    image: 'images/bronn.png'
+  },
+  {
+    name: 'Cersei Lannister',
+    image: 'images/cerseiLannister.png'
+  },
+  {
+    name: 'Daario Naharis',
+    image: 'images/daarioNaharis.png'
+  },
+  {
+    name: 'Davos Seaworth',
+    image: 'images/davosSeaworth.png'
+  },
+  {
+    name: 'Denerys Targaryan',
+    image: 'images/denerysTargaryan.png'
+  },
+  {
+    name: 'Euron Grayjoy',
+    image: 'images/euronGreyjoy.png'
+  },
+  {
+    name: 'Gendry',
+    image: 'images/gendry.png'
+  },
+  {
+    name: 'Gilly',
+    image: 'images/gilly.png'
+  },
+  {
+    name: 'Grey Worm',
+    image: 'images/greyWorm.png'
+  },
+  {
+    name: 'Jamie Lannister',
+    image: 'images/jamieLannister.png'
+  },
+  {
+    name: 'Jaqen Hghar',
+    image: 'images/jaqenHghar.png'
+  },
+  {
+    name: 'Podrick Payne',
+    image: 'images/podrickPayne.png'
+  },
+  {
+    name: 'Qyburn',
+    image: 'images/qyburn.png'
+  },
+  {
+    name: 'Samwell Tarly',
+    image: 'images/samwellTarly.png'
+  },
+  {
+    name: 'Sansa Stark',
+    image: 'images/sansaStark.png'
+  },
+  {
+    name: 'The Hound',
+    image: 'images/theHound.png'
+  },
+  {
+    name: 'The Mountain',
+    image: 'images/theMountain.png'
+  },
+  {
+    name: 'Tormund Giantsbane',
+    image: 'images/tormundGiantsbane.png'
+  },
+  {
+    name: 'Tyrion Lannister',
+    image: 'images/tyrionLannister.png'
+  },
+  {
+    name: 'Varys',
+    image: 'images/varys.png'
+  },
+  {
+    name: 'Yara Greyjoy',
+    image: 'images/yaraGreyjoy.png'
+  },
+  {
+    name: 'Arya Stark',
+    image: 'images/aryaStark.png'
   }
 ]
 
 function initAll() {
-  var boksi = document.getElementById('boksi');
+  var boksi = document.getElementById("boksi");
   console.log(arrayOfObject[0])
   document.getElementById('teksti').innerHTML = arrayOfObject[0].name;
   boksi.src = arrayOfObject[0].image;
@@ -22,21 +118,42 @@ function initAll() {
   }
 }
 
+//TODO: Proper iteration through the damn shitfuck
 
 function newCard() {
-  for(var i=0; i<24 ; i++){
-    setSquare(i);
-  }
+  var table = document.getElementById("table");
+    for (var i = 0, row; row = table.rows[i]; i++) {
+   //iterate through
+   console.log(row+" row "+i)
+   //rows would be accessed using the "row" variable assigned in the for loop
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        console.log(col+" col "+j)
+
+
+
+     //iterate through columns
+     //columns would be accessed using the "col" variable assigned in the for loop
+   }
+ }
+}
+
+function setImage(row, column){
+  var currentSquare = "sq" + row + column
+  var currentImage = currentSquare + "i"
+  console.log(currentImage)
+
+  var imageSource = document.getElementById(currentImage);
+
+  imageSource.src = "images/jonSnow.png"
+  console.log(imageSource)
 }
 
 function setSquare(thisSquare){
   var currentSquare = "sq" + thisSquare;
-  var colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4);
-  var colBasis = colPlace[thisSquare] * 15;
-  var newNum = colBasis + getNewNum() + 1;
-
+  var newNum = getNewNum();
+  var currentSquareImage = "sq"+thisSquare+"i"
   do{
-    newNum = colBasis + getNewNum() + 1;
+    newNum = getNewNum();
   }while(usedNums[newNum]);
 
   usedNums[newNum] = true;
@@ -45,7 +162,9 @@ function setSquare(thisSquare){
 }
 
 function getNewNum() {
-  return Math.floor(Math.random() * 15);
+  var min = 0
+  var max = 24
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function anotherCard() {
